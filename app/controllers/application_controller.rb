00a9c1d373a3ccb4524165f9eaa591c3606e5e96
Mevershed <matthew@evershed.ca>
2017-02-27 18:27:50 -0500

    current user method in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  def current_user
+      return unless session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   protect_from_forgery with: :exception
 end
