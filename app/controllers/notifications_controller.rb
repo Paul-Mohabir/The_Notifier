7ed8038941897b4824bdbc1fce6f5b93a019221c
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-27 18:22:24 -0500

    adding the notifications controller...methods  listed are new show create delete

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,22 @@
 class NotificationsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @notification = Notification.new
+  end
+
+  def create
+    @notification = Notification.new(notification_params)
+  end
+
+  def delete
+  end
+
+  private
+
+  def notification_params
+    params.require(:user).permit 
+
 end
