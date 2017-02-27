11caec606412b29cc543874bc0ba563cdebc53dc
Joseph Papaluca <jpapaluc@gmail.com>
2017-02-27 18:24:40 -0500

    added methods in UserSessions controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,19 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    if @user = login(params[:email], params[:crypted_password], params[:remember_me])
+      redirect_back_or_to(root_url, notice: "Logged In!")
+    else
+      flash.now[:alert] = "Login Failed!"
+      render action: 'new'
+    end
   end
 
   def destroy
+    logout
+    redirect_to root_url, notice: "Logged Out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
 
   root 'pages_controller#index'
 
   resources :users, :notifications
+  resources :user_sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
