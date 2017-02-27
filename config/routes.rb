6263c2fe11c8df74c0dea46639f2dceb62f5156c
Mevershed <matthew@evershed.ca>
2017-02-27 18:12:15 -0500

    generate notifications controller and new and show .erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages_controller#index'
 
-  resources :users, :notfications
+  resources :users, :notifications
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
