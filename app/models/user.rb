3a9be204cf5f5b62b7ec44d4cf9d4655bcbe6ae0
Mevershed <matthew@evershed.ca>
2017-02-28 13:33:12 -0500

    some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-
+  # validates :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates_presence_of :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates :email, uniqueness: true
 end
