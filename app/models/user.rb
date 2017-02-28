295fca6186f6059eaa656a48593445714d20dd45
Joseph Papaluca <jpapaluc@gmail.com>
2017-02-27 22:17:54 -0500

    filled out user_sessions controller; created login form, added notices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
 
   validates_presence_of :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates :email, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   resources :users, :notifications
   resources :user_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
