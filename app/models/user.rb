0b7b23158ecf62bb25fd1ddd40c97a28bd7ba188
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-03-01 11:25:21 -0500

    added associations to user and schedules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_one :Schedule
+  has_one :Shift      
   # validates :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates_presence_of :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates :email, uniqueness: true
