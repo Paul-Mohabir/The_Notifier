22fe1f5b03c191c699917bebeacdcfd30d5d4d67
Mevershed <matthew@evershed.ca>
2017-03-02 15:47:11 -0500

    updated notification, shift and user models to work with the new assosciations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  has_one :Schedule
-  has_one :Shift      
+  has_and_belongs_to_many :Shifts
+  has_many :Notifications through :Shifts
   # validates :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates_presence_of :name, :address, :phone, :emergency_contact, :shift_preference, :password
   validates :email, uniqueness: true
