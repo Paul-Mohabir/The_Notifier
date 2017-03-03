5ee9d176236e0b93628646c6a6e144ab3310445f
Mevershed <matthew@evershed.ca>
2017-03-03 18:38:44 -0500

    calendar formatting fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  # before_filter :set_time_zone, if: :user_signed_in?
   def current_user
       return unless session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end
   protect_from_forgery with: :exception
+  private
+
+    def set_time_zone
+      Time.zone = current_user.time_zone
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-config.beginning_of_week = :sunday
-config.time_zone = 'Central Time (US & Canada)'
+# you've limited to :test, :development, or :production
 Bundler.require(*Rails.groups)
 module Notify
   class Application < Rails::Application
