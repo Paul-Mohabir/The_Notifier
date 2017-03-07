288ba4569708f9e243dc05702f29b2e187d68c80
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-03-07 17:31:04 -0500

    changed assoications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   belongs_to :Company
-  has_and_belongs_to_many :Shifts
-  has_many :Notifications, through: :Shifts
+  belongs_to :Calendar
+  has_many :Notifications, through: :Company
   validates_presence_of :name, :address, :phone, :emergency_contact, :shift_preference, :password, :company_id
   validates :email, uniqueness: true
 end
