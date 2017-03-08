6b19fd8a184aa6b647398997d82cf07d00091ebe
Mevershed <matthew@evershed.ca>
2017-03-08 11:15:01 -0500

    not sure

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,25 @@
 class CompaniesController < ApplicationController
-  def show
+
+  def index
+    @companies = Company.all
   end
+
+  def new
+    @company = Company.new
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def create
+    @company = Company.new(company_params)
+    
+  end
+
+  private
+
+    def company_params
+      params.require(:company).permit(:name)
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
   has_one :Calendar
-  has_many :Users
+  has_many :Users, :foreign_key => 'user_id'
   validates_presence_of :name
 end
