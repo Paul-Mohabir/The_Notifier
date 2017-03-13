b8fd14f28307fbbc7c450cbf47cd6c5ad5c50821
Joseph Papaluca <jpapaluc@gmail.com>
2017-03-13 18:49:55 -0400

    added employee list to company page; working on company edit form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,27 +4,28 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+    if current_user.admin == false && current_user.company_id != @company.id
+      redirect_to root_path
+    end
+  end
+
   def new
     @company = Company.new
   end
 
-  def show
-    @company = Company.find(params[:id])
-
-    if current_user.admin == false && current_user.company_id != @company.id
-      redirect_to root_path
-    end
-    
+  def edit
   end
+
 
   def create
     @company = Company.new(company_params)
-
   end
 
   private
 
     def company_params
-      params.require(:company).permit(:name)
+      params.require(:company).permit(:name, :location, :contact_info, :picture)
     end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
-  has_one :Calendar
-  has_many :Users
-  has_many :Notications
+  has_one :calendar
+  has_many :users
+  has_many :notications
   validates_presence_of :name
 end
